7de90e91a17184f409a1f64310d45c225665d28f
abner rivera <abner.rivera@150porciento.com>
2023-09-14 11:48:53 -0500

    update front nav and banner

--- app/app.js
+++ app/app.js
@@ -4,3 +4,18 @@ var swiper = new Swiper(".SwiperTrainer", {
     //     el: ".swiper-pagination",
     // },
 });
+
+//menu mobile
+const menuIcon = document.getElementById('menu-mobile')
+const links = document.getElementById('nav-links')
+
+// toggle menu
+const toggleMenu = () => {
+    links.classList.toggle('active')
+    menuIcon.classList.toggle('active')
+}
+
+menuIcon.addEventListener('click', toggleMenu)
+links.childNodes.forEach(item => {
+    item.addEventListener('click', toggleMenu)
+})
